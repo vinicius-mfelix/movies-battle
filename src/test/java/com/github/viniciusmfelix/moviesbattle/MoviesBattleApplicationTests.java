f0700981c836b3072c8be75bc7766a781a446f0c
vinicius-mfelix <vinicius.mfelix@icloud.com>
2023-04-03 18:59:29 -0300

    Initial commit

---
+++ src/test/java/com/github/viniciusmfelix/moviesbattle/MoviesBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusmfelix.moviesbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
